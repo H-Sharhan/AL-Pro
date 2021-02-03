ee12b5edbfe03222edf8e7496f3e13f4289d9fbe
H-Sharhan <78044231+H-Sharhan@users.noreply.github.com>
2021-02-03 14:57:17 +0000

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        long start=System.currentTimeMillis();
+        long end,time;
+
+        int sum=10000*(10000+1)/2;
+        System.out.println(sum );
+
+ time=end=System.currentTimeMillis()-start;
+     System.out.println(time);
+    }
+}
